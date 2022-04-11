71ae372ccb45b6f8b90fcb44fb02237817384fe6
Shokri <shokre4@hotmail.com>
2022-04-11 23:46:37 +0400

    Initial Commit

---
+++ rockPaperScissor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rockPaperScissor
+//
+//  Created by Shokri Alnajjar on 11/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rockPaperScissor/rockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorApp.swift
+//  rockPaperScissor
+//
+//  Created by Shokri Alnajjar on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
